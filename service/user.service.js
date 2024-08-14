2c1df09b4480a6e0a3f6e1ba36cd11849f782821
unknown <rare7287@gamil.com>
2024-08-14 11:40:06 +0900

    dev 브랜치 최신화

--- service/user.service.js
+++ service/user.service.js
@@ -1,18 +1,30 @@
+//가지고있는 정보 활용
 const User = require("../schema/user.schema");
 //user = {emial:"", nickname="", password=""}
 const createUser = async (user) => {
-    try{
-        const createUser = await User.create(user);
-        console.log(createUser);
-    }catch(err){
-        console.log(err);
-    }
+  try {
+    const createUser = await User.create(user);
+    console.log(createUser);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
-const getUser = async (email, password, nickname) =>{
-    const user = await User.findOne({});
+const getUser = async (email, password, nickname) => {
+  const user = await User.findOne({});
 };
+
+const getUserByEmail = async (email) => {
+  try {
+    const user = await User.findOne({ email });
+    console.log(user);
+    return user;
+  } catch (err) {
+    return null;
+  }
+}; //유저 정보를 가져옴
 
 module.exports = {
-    createUser,
+  createUser,
+  getUserByEmail,
 };
